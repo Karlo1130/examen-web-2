f58b1c53bb4b4f882873061ebe4e67dc40f3f99f
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2023-12-02 16:22:36 -0700

    Proyecto actualizado (Datos del LogIn obtenidos)

--- proyecto/proyectoApp/views.py
+++ proyecto/proyectoApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,15 @@ def index(request):
 
 def login(request):
     return render(request, "login.html")
+
+def signUp(request):
+
+    if request.method == "GET":
+        print ("enviando formulario")
+    else:
+        print(request.POST)
+    return render(request, "signUp.html")
+
+def buscar(request):
+    nombre = "Nombre: %r" %request.GET["Nombres"]
+    return HttpResponse(nombre)
